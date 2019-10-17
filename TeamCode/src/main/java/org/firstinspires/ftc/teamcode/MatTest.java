2df0c367906fb3f4fd32cb3e0679ae371b4d6608
Matthew Freestone <matthewfreestone1593@gmail.com>
2019-10-16 19:09:15 -0500

    updated version and added init line

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MatTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MatTest.java
@@ -3,24 +3,26 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
 @TeleOp(name="MatTest", group="Computer Vision")
 public class MatTest extends LinearOpMode {
-    Mat mat = new Mat(5, 5, CvType.CV_8UC4);
+    boolean useless = OpenCVLoader.initDebug();
+
     @Override
     public void runOpMode() {
         // run until the end of the match (driver presses STOP)
-        telemetry.addData("started","started");
+        telemetry.addData("startSuccess",useless);
         telemetry.update();
-
 
         waitForStart();
 
-        telemetry.addData("trying", "declaration");
+        telemetry.addData("trying", "assignment");
         telemetry.update();
         sleep(2000);
+        Mat mat = new Mat(5, 5, CvType.CV_8UC4);
         //Mat mat;
         telemetry.addData("trying", "assignment");
         telemetry.update();
