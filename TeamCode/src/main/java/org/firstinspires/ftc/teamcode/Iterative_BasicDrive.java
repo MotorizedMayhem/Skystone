afc1d83686c9287f5a1dfc12753b0bcb1e6b8001
Matthew Freestone <matthewfreestone1593@gmail.com>
2019-11-14 23:34:35 -0600

    created hardware class for iterative opmodes and created a simple drive demonstration

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Iterative_BasicDrive.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.teamcode.MM_Classes.MM_IterativeOpMode;
+
+public class Iterative_BasicDrive extends MM_IterativeOpMode {
+    @Override
+    public void init() {
+        super.init();
+    }
+
+
+    @Override
+    public void loop(){
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y) * .4;
+        double radians = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double turn = -gamepad1.right_stick_x * .4;
+        final double v1 = r * Math.cos(radians) - turn;
+        final double v2 = r * Math.sin(radians) + turn;
+        final double v3 = r * Math.sin(radians) - turn;
+        final double v4 = r * Math.cos(radians) + turn;
+        robot.motor_powers(v1, v2, v3, v4);
+        telemetry.addData("Steering","Mag: %.2f, Theta: %.1f deg", r, (radians*180)/Math.PI);
+        telemetry.addData("Motor Powers", "fl: %.3f, fr: %.3f, br: %.3f, bl: %.3f", v1,v2,v3,v4);
+    }
+}
